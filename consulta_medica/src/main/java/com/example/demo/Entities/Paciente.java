01c8152d9fc63833dff7673cdf841eb11aa3474a
MateusSpoliante <mateus.spoliant11@gmail.com>
2025-05-19 09:04:40 -0300

    API_Consulta_Medica

---
+++ consulta_medica/src/main/java/com/example/demo/Entities/Paciente.java
@@ -0,0 +1,32 @@
+package com.example.demo.Entities;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "pacientes")
+@AllArgsConstructor @NoArgsConstructor
+public class Paciente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private String email;
+
+    
+}
---
+++ consulta_medica/src/main/java/com/example/demo/controller/PacienteController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+/* 
+ import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Pacientes", description = "Endpoints para gerenciamento de pacientes")
+@RestController
+@RequestMapping("api/pacientes")
+public class PacienteController {
+   
+    
+    @Autowired
+    private PacienteService pacienteService;
+
+    public ResponseEntity<List<PacienteDTO>> listarPacientes() {
+        List<PacientesDTO> pacientes = pacienteService.listarTodos();
+        return ResponseEntity.ok(pacientes);
+    }
+
+}
+*/
+
